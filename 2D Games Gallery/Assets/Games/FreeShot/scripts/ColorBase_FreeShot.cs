0e039497aa48e17aee66eb4991b61e9fbc521d4a
lickey10 <lickey10@gmail.com>
2019-06-22 23:46:09 -0600

    first import

---
+++ 2D Games Gallery/Assets/Games/FreeShot/scripts/ColorBase_FreeShot.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// Base class for ColorBrightLogic and ColorDarkLogic
+/// </summary>
+public class ColorBase_FreeShot : MonoBehaviour 
+{
+	public SpriteRenderer sr;
+	public Camera cam;
+	public Image im;
+	public Text txt;
+
+	public float timeAnim = 0.3f;
+
+	void Awake()
+	{
+		sr = GetComponent<SpriteRenderer>();
+		cam = GetComponent<Camera>();
+		im = GetComponent<Image>();
+		txt = GetComponent<Text>();
+	}
+
+	void OnEnable()
+	{
+		GameManager_FreeShot.OnColorChanged += OnColorChanged;
+	}
+
+	void OnDisable()
+	{
+		GameManager_FreeShot.OnColorChanged -= OnColorChanged;
+	}
+
+	public virtual void OnColorChanged (AAColor_FreeShot c)
+	{
+
+	}
+
+	public virtual void SetColor(Color c)
+	{
+		if(cam != null)
+			cam.backgroundColor = c;
+
+		if(sr != null)
+			sr.color = c;
+
+		if(im != null)
+			im.color = c;
+
+		if(txt != null)
+			txt.color = c;
+	}
+}
